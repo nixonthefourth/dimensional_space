ab774675371ddb31e9237596ca5d8646c49e5ae5
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-10-17 17:45:58 +0100

    feat: creating runtime loop as well as the setup for the colours, screen constants

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 """Imports"""
 import pygame as pg
 import sys
-import time
 import numpy as np
+
+'''Constants'''
+BLACK, WHITE = (0, 0, 0), (242, 240, 239)
+WIDTH, HEIGHT = 800, 600
+
+"""PyGame Setup"""
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption('Dimensional Space')
+clock = pg.time.Clock()
 
 """Models"""
 
-'''Test Cube'''
+'''Simple Cube'''
 
 # Vertices
 vertices = np.array([
@@ -28,3 +37,21 @@ edges = np.array([
     (4, 5), (5, 7), (7, 6), # Front
     (5, 1), (1, 3), (3, 7) # East
 ])
+
+"""Runtime Loop"""
+while True:
+
+    # Exit Sequence
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit()
+
+    screen.fill(BLACK)
+
+    # Framerate Update
+    pg.display.flip()
+    clock.tick(60)
+
+# Shuts Down Engine
+pg.quit()
+sys.exit()
