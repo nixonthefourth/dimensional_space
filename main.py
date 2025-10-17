3968be4e45694c76432dc244df9a6a22aba6f95c
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-10-17 17:35:47 +0100

    feat: Created vertices and edges for a 3D cube. Applied arrays and tuples to create multi-dimensional matrices.

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+"""Imports"""
 import pygame as pg
 import sys
 import time
 import numpy as np
+
+"""Models"""
+
+'''Test Cube'''
+
+# Vertices
+vertices = np.array([
+    (-1, 1, -1), # V1
+    (1, 1, -1), # V2
+    (-1, -1, -1), # V3
+    (1, -1, 1), # V4
+
+    (-1, 1, 1), # V5
+    (1, 1, 1), # V6
+    (-1, -1, 1), # V7
+    (1, -1, 1) # V8
+])
+
+# Edges
+edges = np.array([
+    (0, 1), (1, 3), (3, 2), (3, 0), # Rear
+    (0, 4), (4, 6), (6, 2), # West
+    (4, 5), (5, 7), (7, 6), # Front
+    (5, 1), (1, 3), (3, 7) # East
+])
