707ad202910084409541cfb9cb248b89a33c7ee2
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-10-17 18:13:18 +0100

    feat: adding projection and focal length calculations

--- main.py
+++ main.py
@@ -38,6 +38,46 @@ edges = np.array([
     (5, 1), (1, 3), (3, 7) # East
 ])
 
+"""Functions"""
+
+'''Focal Length Calculation'''
+def calc_focal_length(d, fov):
+    f = (d * np.arctan(np.radians(fov) / 2)) / 2
+    return f
+
+'''Project Object'''
+
+# Individual Point Projection
+def project_point(point, f, width, height):
+    x, y, z = point
+
+    # Principal Points
+    c_x, c_y = width / 2, height / 2
+
+    # z can't mathematically be 0
+    if z <= 0:
+        z = 0.00001
+
+    # Calculate each projected point
+    x_proj = (f * x) / z + c_x
+    y_proj = (f * y) / z + c_y
+
+    return np.array([x_proj, y_proj])
+
+# Multiple Points Projection
+def project_points(points, f, width, height):
+    proj_points = []
+
+    for i in points:
+        x_proj, y_proj = project_point(i, f, width, height)
+
+        u = width / 2 + x_proj
+        v = height / 2 - y_proj
+
+        proj_points.append(np.array([u, v]))
+
+    return proj_points
+
 """Runtime Loop"""
 while True:
 
